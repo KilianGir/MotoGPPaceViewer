f58aa60ff7831d1bde07492fab10723f484bf17f
KilianGir <97527152+KilianGir@users.noreply.github.com>
2022-03-10 11:47:01 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,6 @@
 import pandas as pd
 from matplotlib import pyplot as plt
 from matplotlib.pyplot import figure
-import seaborn as sns
 import numpy as np
 import plotly.graph_objects as go
 import streamlit as st
